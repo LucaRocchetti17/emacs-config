0756cb8b72534d5c761bc2a0e3d4c7a06a48d4ab
Luca Rocchetti <rocchetti.1757075@studenti.uniroma1.it>
2023-10-21 14:22:48 +0200

    init file cleaned

--- gnu-linux-29.1/init.el
+++ gnu-linux-29.1/init.el
@@ -33,23 +33,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (goose)))
- '(custom-safe-themes
-   (quote
-	("36331aa9a054f3e8a2b7e3a1139287e3159596b2ad4166e8e0d47928282f8e77" "1dacaddeba04ac1d1a2c6c8100952283b63c4b5279f3d58fb76a4f5dd8936a2c" default)))
- '(ido-create-new-buffer (quote always))
- '(ido-mode (quote both) nil (ido))
- '(ido-vertical-define-keys (quote C-n-and-C-p-only))
- '(ido-vertical-mode t)
- '(package-selected-packages
-   (quote
-	(which-key use-package switch-window swiper sudo-edit smex s rainbow-mode rainbow-delimiters popup-kill-ring page-break-lines org-bullets mark-multiple linum-relative ido-vertical-mode goose-theme fill-column-indicator expand-region diminish dash beacon avy))))
+ '(custom-enabled-themes '(modus-operandi)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ido-subdir ((t (:foreground "black"))))
  )
