595018efd4faea0a4dead8b5795d0f8096e36b57
Luca Rocchetti <rocchetti.1757075@studenti.uniroma1.it>
2023-10-21 15:52:40 +0200

    Gnu-linux 27.1 version added

---
+++ gnu-linux-27.1/init.el
@@ -0,0 +1,53 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+(require 'package)
+
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(
+             ;;("ELPA"  . "http://tromey.com/elpa/")
+			 ;;("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ;;("org"   . "http://orgmode.org/elpa/")
+             ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes '(goose))
+ '(custom-safe-themes
+   '("36331aa9a054f3e8a2b7e3a1139287e3159596b2ad4166e8e0d47928282f8e77" "1dacaddeba04ac1d1a2c6c8100952283b63c4b5279f3d58fb76a4f5dd8936a2c" default))
+ '(ido-create-new-buffer 'always)
+ '(ido-mode 'both nil (ido))
+ '(ido-vertical-define-keys 'C-n-and-C-p-only)
+ '(ido-vertical-mode t)
+ '(package-selected-packages
+   '(go-mode xah-fly-keys which-key use-package switch-window swiper sudo-edit smex s rainbow-mode rainbow-delimiters popup-kill-ring page-break-lines org-bullets mark-multiple linum-relative ido-vertical-mode goose-theme fill-column-indicator expand-region diminish dash beacon avy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-subdir ((t (:foreground "black"))))
+ '(org-todo ((t (:foreground "dark red" :weight bold)))))
