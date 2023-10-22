11813653b0c3e85e59fe6b2b2efbcbe6389ddb58
Luca Rocchetti <rocchetti.1757075@studenti.uniroma1.it>
2023-10-22 09:09:07 +0200

    Few modification init file

--- gnu-linux-27.1/init.el
+++ gnu-linux-27.1/init.el
@@ -8,10 +8,10 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives '(
-             ;;("ELPA"  . "http://tromey.com/elpa/")
-			 ;;("gnu"   . "http://elpa.gnu.org/packages/")
+             ("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ;;("org"   . "http://orgmode.org/elpa/")
+			 ("org"   . "http://orgmode.org/elpa/")
              ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 
 (package-initialize)
@@ -35,15 +35,15 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes '(goose))
+ '(custom-enabled-themes '(modus-operandi))
  '(custom-safe-themes
-   '("36331aa9a054f3e8a2b7e3a1139287e3159596b2ad4166e8e0d47928282f8e77" "1dacaddeba04ac1d1a2c6c8100952283b63c4b5279f3d58fb76a4f5dd8936a2c" default))
+   '("3d94d6d1a1c23113a60c8496c9aed094dbc2695f219e8127bb168d17b1e6dab3" "4b026ac68a1aa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" "36331aa9a054f3e8a2b7e3a1139287e3159596b2ad4166e8e0d47928282f8e77" "1dacaddeba04ac1d1a2c6c8100952283b63c4b5279f3d58fb76a4f5dd8936a2c" default))
  '(ido-create-new-buffer 'always)
  '(ido-mode 'both nil (ido))
  '(ido-vertical-define-keys 'C-n-and-C-p-only)
  '(ido-vertical-mode t)
  '(package-selected-packages
-   '(go-mode xah-fly-keys which-key use-package switch-window swiper sudo-edit smex s rainbow-mode rainbow-delimiters popup-kill-ring page-break-lines org-bullets mark-multiple linum-relative ido-vertical-mode goose-theme fill-column-indicator expand-region diminish dash beacon avy)))
+   '(modus-themes go-mode xah-fly-keys which-key use-package switch-window swiper sudo-edit smex s rainbow-mode rainbow-delimiters popup-kill-ring page-break-lines org-bullets mark-multiple linum-relative ido-vertical-mode goose-theme fill-column-indicator expand-region diminish dash beacon avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
