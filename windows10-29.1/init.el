65727c983188e085865b8b18fc13b07725a93ad9
Luca Rocchetti <luca.rocchetti17@gmail.com>
2023-10-24 10:00:38 +0200

    Win which-key package enabled

--- windows10-29.1/init.el
+++ windows10-29.1/init.el
@@ -35,8 +35,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-operandi))
- '(package-selected-packages
-   '(xah-fly-keys)))
+ '(package-selected-packages '(which-key xah-fly-keys)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
