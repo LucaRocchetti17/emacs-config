4f36a5dc038325bb7696e3900ffc04d979327c4d
Luca Rocchetti <rocchetti.1757075@studenti.uniroma1.it>
2023-12-15 08:37:50 +0100

    gnu 27.1 vue mode added

--- gnu-linux-27.1/init.el
+++ gnu-linux-27.1/init.el
@@ -33,21 +33,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
  '(custom-enabled-themes '(modus-operandi))
  '(custom-safe-themes
-   '("3d94d6d1a1c23113a60c8496c9aed094dbc2695f219e8127bb168d17b1e6dab3" "4b026ac68a1aa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" "36331aa9a054f3e8a2b7e3a1139287e3159596b2ad4166e8e0d47928282f8e77" "1dacaddeba04ac1d1a2c6c8100952283b63c4b5279f3d58fb76a4f5dd8936a2c" default))
+   '("f5661fd54b1e60a4ae373850447efc4158c23b1c7c9d65aa1295a606278da0f8" "3d94d6d1a1c23113a60c8496c9aed094dbc2695f219e8127bb168d17b1e6dab3" "4b026ac68a1aa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" "36331aa9a054f3e8a2b7e3a1139287e3159596b2ad4166e8e0d47928282f8e77" "1dacaddeba04ac1d1a2c6c8100952283b63c4b5279f3d58fb76a4f5dd8936a2c" default))
  '(ido-create-new-buffer 'always)
  '(ido-mode 'both nil (ido))
  '(ido-vertical-define-keys 'C-n-and-C-p-only)
  '(ido-vertical-mode t)
  '(package-selected-packages
-   '(modus-themes go-mode xah-fly-keys which-key use-package switch-window swiper sudo-edit smex s rainbow-mode rainbow-delimiters popup-kill-ring page-break-lines org-bullets mark-multiple linum-relative ido-vertical-mode goose-theme fill-column-indicator expand-region diminish dash beacon avy)))
+   '(vue-mode yaml-mode modus-themes go-mode xah-fly-keys which-key use-package switch-window swiper sudo-edit smex s rainbow-mode rainbow-delimiters popup-kill-ring page-break-lines org-bullets mark-multiple linum-relative ido-vertical-mode goose-theme fill-column-indicator expand-region diminish dash beacon avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ido-subdir ((t (:foreground "black"))))
- '(org-todo ((t (:foreground "dark red" :weight bold)))))
+ )
